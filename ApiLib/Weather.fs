1eebbaa001110f48e6351df5e06f67bf682d7c87
Yakimych <yakimych@gmail.com>
2020-05-04 09:10:17 +0200

    Move code to lib and use in WebApp

---
+++ ApiLib/Weather.fs
@@ -0,0 +1,29 @@
+﻿namespace ApiLib.Weather
+
+open System
+
+module WeatherLogic =
+
+    type WeatherForecast =
+        { Date: DateTime
+          TemperatureC: int
+          Summary: string }
+
+    let summaries =
+        [| "Freezing"
+           "Bracing"
+           "Chilly"
+           "Cool"
+           "Mild"
+           "Warm"
+           "Balmy"
+           "Hot"
+           "Sweltering"
+           "Scorching" |]
+
+    let getForecast () =
+        let rng = System.Random()
+        [| for index in 0 .. 4 ->
+            { Date = DateTime.Now.AddDays(float index)
+              TemperatureC = rng.Next(-20, 55)
+              Summary = summaries.[rng.Next(summaries.Length)] } |]
--- WebApp/Controllers/WeatherForecastController.fs
+++ WebApp/Controllers/WeatherForecastController.fs
@@ -1,26 +1,12 @@
 ﻿namespace WebApp.Controllers
 
-open System
-open System.Collections.Generic
-open System.Linq
-open System.Threading.Tasks
 open Microsoft.AspNetCore.Mvc
-open Microsoft.Extensions.Logging
-open WebApp
+open ApiLib.Weather
 
 [<ApiController>]
 [<Route("[controller]")>]
-type WeatherForecastController (logger : ILogger<WeatherForecastController>) =
+type WeatherForecastController() =
     inherit ControllerBase()
 
-    let summaries = [| "Freezing"; "Bracing"; "Chilly"; "Cool"; "Mild"; "Warm"; "Balmy"; "Hot"; "Sweltering"; "Scorching" |]
-
     [<HttpGet>]
-    member __.Get() : WeatherForecast[] =
-        let rng = System.Random()
-        [|
-            for index in 0..4 ->
-                { Date = DateTime.Now.AddDays(float index)
-                  TemperatureC = rng.Next(-20,55)
-                  Summary = summaries.[rng.Next(summaries.Length)] }
-        |]
+    member __.Get(): WeatherLogic.WeatherForecast [] = WeatherLogic.getForecast ()
